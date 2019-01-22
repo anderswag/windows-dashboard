ed0d3dc257311c5b67da80e10e38e2ba6ad499bb
Anderson <anderson.hung@live.com>
2019-01-22 10:47:02 -0500

    add header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,9 @@ class Header extends Component {
   render() {
     return (
       <div className="header">
-        <div className="header-item" />
+        <div className="header-item">
+          <p className="header-item-text">Start</p>
+        </div>
         <div className="header-item" />
       </div>
     );
